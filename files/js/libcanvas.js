bf3e7a6cb5bd56ee0d7b3304b5a7b4f4ffc7a317
Shock <shocksilien@gmail.com>
2011-04-30 05:10:12 +0300

    added e.preventDefault()

--- files/js/libcanvas.js
+++ files/js/libcanvas.js
@@ -1133,7 +1133,7 @@ LibCanvas.Mouse = atom.Class({
 	setEvents : function () {
 		var mouse = this, waitEvent = function (event, isOffise) {
 			return function (e) {
-				atom.dom().create('p').html('e+'+event).appendTo(document);
+				atom.dom().create('p').html('e+'+event).appendTo('body');
 				var wait = mouse.isEventAdded(event);
 				if (isOffise || wait) mouse.getOffset(e);
 				if (isOffise) mouse.events.event(event, e);
@@ -1144,7 +1144,7 @@ LibCanvas.Mouse = atom.Class({
 		};
 
 		var move = function (e) {
-			atom.dom().create('p').html('move').appendTo(document);
+			atom.dom().create('p').html('move').appendTo('body');
 			var offset = mouse.getOffset(e);
 			mouse.setCoords(offset);
 			mouse.events.event('mousemove', e);
@@ -1154,7 +1154,7 @@ LibCanvas.Mouse = atom.Class({
 		};
 
 		var out = function (e) {
-			atom.dom().create('p').html('out').appendTo(document);
+			atom.dom().create('p').html('out').appendTo('body');
 			mouse.getOffset(e);
 			mouse.setCoords(null);
 			mouse.events.event('mouseout', e);
