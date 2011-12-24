9c44ad5a36eee916ae672ca09ec51cd3adab1886
Shock <shocksilien@gmail.com>
2011-12-24 23:26:25 +0300

    correct touch

--- games/mines/js/controller.js
+++ games/mines/js/controller.js
@@ -57,7 +57,7 @@ Mines.Controller = atom.Class({
 		// libcanvas.wrapper.bind( 'touchstart', prevent );
 		// libcanvas.wrapper.bind( 'touchmove', prevent );
 		libcanvas.wrapper.bind( 'touchend' , function (e) {
-			new Trace(e.offsetX, e.offsetY);
+			new Trace(e.changedTouches[0].offsetX, e.changedTouches[0].offsetY);
 			field.action( LibCanvas.Mouse.getOffset(e) , controller.isFlagAction() );
 			e.preventDefault();
 		});
