66db386389fb98d14c546d1525af542f833a36b0
TheShock <shocksilien@gmail.com>
2012-02-21 06:02:40 +0200

    Fifteen example

---
+++ games/fifteen/field.js
@@ -0,0 +1,160 @@
+atom.declare( 'Fifteen.Field', {
+	initialize: function (settings) {
+		var app, scene, mouse;
+
+		this.settings = new atom.Settings( settings );
+
+		app = new LibCanvas.App({
+			size: this.size()
+		});
+
+		mouse = new Mouse(app.container.bounds);
+		this.mouseHandler = new App.MouseHandler({ mouse: mouse, app: app });
+		
+		scene = app.createScene({ name: 'cells', intersection: 'manual' });
+		this.generate( scene );
+		this.bindTouch();
+
+		this.activate(false);
+		this.shuffle(100);
+	},
+
+	bindTouch: function () {
+		var tiles = this.tiles;
+		document.addEventListener( 'touchstart', function (e) {
+			var
+				offset = Mouse.getOffset(e),
+				y, x, t;
+
+			for (y in tiles) for (x in tiles[y]) {
+				t = tiles[y][x];
+				if (t && t.hasMousePoint(offset)) {
+					this.move(t);
+					break;
+				}
+			}
+			e.preventDefault();
+		}.bind(this), false);
+
+		var prevent = function(e){e.preventDefault()};
+		document.addEventListener( 'touchmove', prevent, false );
+		document.addEventListener( 'touchend' , prevent, false );
+	},
+
+	move: function (tile, onFinish, fast) {
+		if (tile.activated && !this.blocked) {
+			var empty = this.empty, position = tile.position;
+
+			this.blocked = true;
+			this.activate( true );
+
+			tile.moveTo( this.translatePoint( empty ), function () {
+				var newPosition = empty.clone();
+				empty.moveTo( position );
+				position.moveTo( newPosition );
+				this.activate( false );
+				this.blocked = false;
+				onFinish && onFinish.call( this, tile );
+			}.bind(this), fast);
+		}
+		return this;
+	},
+
+	moveRandom: function (onFinish, previous) {
+		var x, y, t = [], tiles = this.tiles;
+		for (x = 4; x--;) for (y = 4; y--;) {
+			var tile = tiles[y][x];
+			if (tile && tile.activated && tile != previous) t.push( tile );
+		}
+		this.move( t.random, onFinish, true );
+		return this;
+	},
+
+	shuffle: function (times) {
+		var trace = atom.trace( times );
+		var field = this;
+		(function next (tile) {
+			if (times-- > 0) {
+				trace.value = times;
+				field.moveRandom(next, tile);
+			} else {
+				trace.destroy();
+			}
+		})(null);
+		return this;
+	},
+
+	activate: function ( disableAll ) {
+		var y, x, tile;
+		for (y = 0; y < 4; y++) for (x = 0; x < 4; x++) {
+			tile = this.tiles[y][x];
+			if (tile) {
+				tile.activated = disableAll ? false : this.isMovable( tile );
+				tile.redraw();
+			}
+		}
+		return this;
+	},
+
+	isMovable: function ( tile ) {
+		var pos = tile.position, empty = this.empty, diff = pos.diff(empty);
+		return (diff.x == 0 && diff.y.abs() == 1) || (diff.y == 0 && diff.x.abs() == 1);
+	},
+
+	generate: function (scene) {
+		var
+			y, x, position, index,
+			tiles = {},
+			indexes = Array.range( 1, 15 );
+		for (y = 0; y < 4; y++) {
+			tiles[y] = {};
+			for (x = 0; x < 4; x++) {
+				position = new Point( x, y );
+				if (indexes.length) {
+					index = indexes.shift();
+					tiles[y][x] = this.createTile( scene, index, position );
+				} else {
+					this.empty = position;
+				}
+			}
+		}
+
+		this.tiles = tiles;
+		return this;
+	},
+
+	createTile: function (scene, index, position) {
+		var tile = new Fifteen.Tile( scene, {
+			position: position,
+			shape: this.tileShape(position),
+			index: index
+		});
+		this.mouseHandler.subscribe(tile);
+		tile.events.add( 'mousedown', function (e) {
+			e.preventDefault();
+			this.move( tile );
+		}.bind(this));
+
+		return tile;
+	},
+
+	size: function () {
+		var tile = this.settings.get( 'tile' );
+		return new Size(
+			4 * tile.width  + 5 * tile.margin + 1,
+			4 * tile.height + 5 * tile.margin + 1
+		);
+	},
+
+	translatePoint: function (pos) {
+		var size = this.settings.get('tile');
+		return new Point(pos.x * (size.width + size.margin) + size.margin , pos.y * (size.height + size.margin) + size.margin);
+	},
+	
+	tileShape: function (pos) {
+		return new Rectangle({
+			from: this.translatePoint(pos),
+			size: this.settings.get('tile')
+		});
+	}
+});
---
+++ games/fifteen/tile.js
@@ -0,0 +1,101 @@
+atom.declare( 'Fifteen.Tile', {
+	parent: App.Element,
+
+	prototype: {
+		configure: function () {
+			this.animate = new atom.Animatable(this).animate;
+
+			this.sprites = {
+				'standard': this.renderSprite( 'standard' ),
+				'playable': this.renderSprite( 'playable' ),
+				'hover'   : this.renderSprite( 'hover' )
+			};
+
+			this.behaviors = new LibCanvas.Behaviors(this);
+			this.behaviors.add('Clickable', this.redraw).start();
+		},
+
+		get position () {
+			return this.settings.get('position');
+		},
+
+		moveTo: function (destination, onFinish, fast) {
+			var props = {}, current = this.shape.from;
+
+			if ( !destination.x.equals( current.x, 1 ) ) {
+				props.x = destination.x;
+			} else if ( !destination.y.equals( current.y, 1 ) ) {
+				props.y = destination.y;
+			} else {
+				throw new TypeError( 'Wrong destination' );
+			}
+
+			this.animate({
+				time : fast ? 70 : 250,
+				fn   : fast ? 'linear' : 'sine-out',
+				props: props,
+				onTick: this.redraw,
+				onComplete: function () {
+					this.redraw();
+					onFinish && onFinish.call( this );
+				}.bind(this)
+			});
+			return this;
+		},
+
+		get x () { return this.shape.from.x },
+		get y () { return this.shape.from.y },
+
+		set x (value) {
+			return this.shape.move(new Point( value - this.x, 0 ));
+		},
+		set y (value) {
+			return this.shape.move(new Point( 0, value - this.y ));
+		},
+
+		renderSprite: function ( status ) {
+			var
+				buffer = LibCanvas.buffer(this.shape.size, true),
+				shape  = buffer.ctx.rectangle,
+				stroke = shape.clone().snapToPixel();
+
+			var colors = {
+				'standard' : [ '#444', '#111' ],
+				'playable' : [ '#464', '#131' ],
+				'hover'    : [ '#696', '#353' ]
+			}[status];
+
+			buffer.ctx
+				.fill(shape, buffer.ctx.createGradient( shape, {
+					0: colors[0],
+					1: colors[1]
+				}))
+				.text({
+					text: this.settings.get('index'),
+					to  : shape,
+					size: shape.height / 2,
+					color  : 'white',
+					weight : 'bold',
+					align  : 'center',
+					padding: [(shape.height/8).round(), (shape.width/4).round()]
+				})
+				.set({ globalAlpha: 0.6, lineWidth: 1 })
+				.stroke(stroke, '#999');
+
+			return buffer;
+		},
+
+		renderTo: function (ctx) {
+			var status = 'standard';
+			if (this.activated) {
+				status = this.hover ? 'hover' : 'playable';
+			}
+
+			ctx.drawImage({
+				image: this.sprites[ status ],
+				draw : this.shape,
+				optimize: true
+			});
+		}
+	}
+});
